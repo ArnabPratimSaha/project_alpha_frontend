633aae81f2b13cd069e3e022c1fe665f6cebf4ed
arnab pratim saha <arnabpratimsaha@gmail.com>
2021-05-28 19:16:18 +0530

    worked on home page svg

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,21 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Navbar from '../../component/navbar/navbar';
-import ViviFrontPageBot from '../../component/vivifrontpagebot/viviFrontPageBot';
 import useMode from '../../customhooks/useMode'
 import './home.css';
-import DateTimePicker from 'react-datetime-picker';
-var someDate = new Date();
-var numberOfDaysToAdd = 6;
-someDate.setDate(someDate.getDate() + numberOfDaysToAdd); 
+import lottie from "lottie-web";
 
-var someDate2 = new Date();
-someDate.setDate(someDate.getDate() - 1); 
 export default function Home() {
     const [mode,changeMode,MODETYPE,updateMode]=useMode();
     const [value, onChange] = useState(new Date());
     const handleOnModeUpdate=()=>{
         updateMode();
     }
+    const current=useRef(null);
+    useEffect(()=>{
+        lottie.loadAnimation({
+            container: current.current, // the dom element that will contain the animation
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require("./main robot.json")
+          });
+    },[])
     return (
         <div className='home-full-div' style={{backgroundColor:mode===MODETYPE.DARK?'#444':'#cacacaca'}}>
             <Navbar onUpdateMode={handleOnModeUpdate}/>
@@ -24,8 +28,7 @@ export default function Home() {
                     <h1>Let Vivi handle your anouncement's</h1>
                     <h2>forget about forgetting</h2>
                 </div>
-                <div className='bot-div'>
-                    <ViviFrontPageBot/>
+                <div className='bot-div' ref={current}>
                 </div>
                 <div className='home-intro__add-div' style={{color:mode===MODETYPE.DARK?'#fff':'#222'}}>
                     <h1>add vivi to your discord server today</h1>
@@ -34,15 +37,6 @@ export default function Home() {
                 </div>
             </div>
             <div className='home-learn-more' style={{backgroundColor:mode===MODETYPE.DARK?'#555':'#cacaca'}}>
-                <DateTimePicker
-                
-                    onChange={onChange}
-                    maxDetail={"second"}
-                    value={value}
-                    disableClock='true'
-                    maxDate={someDate}
-                    minDate={someDate2}
-                    />
             </div>
         </div>
     )
