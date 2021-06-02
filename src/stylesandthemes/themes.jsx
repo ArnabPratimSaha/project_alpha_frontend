8eb11fa5c3f6183ea36d76de148206e191b7976d
arnab pratim saha <arnabpratimsaha@gmail.com>
2021-06-03 03:06:47 +0530

    worked on dashboard

---
+++ src/stylesandthemes/themes.jsx
@@ -0,0 +1,32 @@
+const DarkONE={
+    backgroundColor:'#333',
+    color:'#fff'
+}
+const LightONE={
+    backgroundColor:'#eeeeee',
+    color:'#222'
+}
+const colorPalettes={
+    VERYDARK:'#000',
+    DARKONE:'#111',
+    DARKTWO:'#222',
+    DARKTHREE:'#333',
+    DARKFOUR:'#444',
+    DARKFIVE:'#313131',
+    DARKSEVEN:'#414141',
+    DARKEIGHT:'#525252',
+    DARKNINE:'#393e46',
+    DARKTEN:'#222831',
+
+    VERYLIGHT:'#fff',
+    LIGHTONE:'#cacaca',
+    LIGHTTWO:'#eeeeee',
+    LIGHTTHREE:'#f9f9f9',
+    LIGHTFOUR:'#fbfbfb',
+    LIGHTFIVE:'#fcf9f4',
+    LIGHTSEVEN:'#f4f9f4',
+    LIGHTEIGHT:'#f7f7f7',
+    LIGHTNINE:'#ded5c4',
+
+}
+export {DarkONE,LightONE,colorPalettes};
